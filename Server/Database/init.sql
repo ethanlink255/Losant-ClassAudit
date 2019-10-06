7342b0c79c9194082c86bd511aff86479a483e94
Ethan Link <ethanlink255@gmail.com>
2019-10-05 20:28:28 -0400

    Corrected SQL init

--- Server/Database/init.sql
+++ Server/Database/init.sql
@@ -2,32 +2,32 @@
 CREATE DATABASE student_log;
 USE student_log;
 CREATE TABLE students(
-    "id" INT(10) PRIMARY KEY NOT NULL,
-    "first_name" VARCHAR(255) NOT NULL,
-    "last_name" VARCHAR(255) NOT NULL
+    id INT(10) PRIMARY KEY NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE classes(
-    "id" INT(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "teacher" VARCHAR(255) NOT NULL
+    id INT(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    teacher VARCHAR(255) NOT NULL
 );
 CREATE TABLE students_out(
-    "id" INT(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    "student_id" INT(10) NOT NULL,
-    "class_id" INT(10) NOT NULL,
-    "time_out" DATE NOT NULL,
-    "destination" VARCHAR(255) NOT NULL
+    id INT(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    student_id INT(10) NOT NULL,
+    class_id INT(10) NOT NULL,
+    time_out DATE NOT NULL,
+    destination VARCHAR(255) NOT NULL
 );
 CREATE TABLE log(
-    "id" INT(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    "student_id" INT(10) NOT NULL,
-    "class_id" INT(10) NOT NULL,
-    "time_out" DATE NOT NULL,
-    "time_in" DATE NOT NULL,
-    "destination" VARCHAR(255) NOT NULL
+    id INT(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    student_id INT(10) NOT NULL,
+    class_id INT(10) NOT NULL,
+    time_out DATE NOT NULL,
+    time_in DATE NOT NULL,
+    destination VARCHAR(255) NOT NULL
 );
 CREATE TABLE users(
-    "id" INT PRIMARY KEY AUTO_INCREMENT,
-    "username" VARCHAR(255) NOT NULL UNIQUE,
-    "password" VARCHAR(255) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
