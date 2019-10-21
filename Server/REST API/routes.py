77a97d88dd7c657949f526096c750477c1399be0
Ethan Link <ethanlink255@gmail.com>
2019-10-21 12:54:43 -0400

    Fixed URL references

--- Server/REST API/routes.py
+++ Server/REST API/routes.py
@@ -15,7 +15,7 @@ def index_dashboards():
     data = []
 
     for _dashboard in dashboards:
-        data.append({"caption" : _dashboard.description, "href" : url_for('dashboard', id = _dashboard.dashboard-id) })
+        data.append({"caption" : _dashboard.description, "href" : url_for('dashboard', id = _dashboard.dashboard-id, _external=True) })
 
     return data
 
@@ -30,34 +30,34 @@ def index():
 @app.route('/dashboard')
 def dashboard():
     if "id" in request.args:
-        return render_template("dashboard.html", id = escape(request.args.get("id"))
+        return render_template("dashboard.html", id = escape(request.args.get("id")))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index', _external=True))
     form = LoginForm()
     
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            return redirect(url_for('login'))
+            return redirect(url_for('login', _external=True))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        return redirect(url_for('index', _external=True))
     
     return render_template('login.html', title="Sign In", form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index', _external=True))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for('login', _external=True))
     return render_template('register.html', title="Register", form = form)
 
 
